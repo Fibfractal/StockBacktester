6b6f0cb96beecf1774e3cc442374e3d57bc7e017
Bandermaster <bandermaster81@hotmail.com>
2019-10-24 12:17:28 +0200

    GraphProject v1

---
+++ GraphProject/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace GraphProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservablePoint>
+                    {
+                        new ObservablePoint(1,1),
+                        new ObservablePoint(2,2),
+                        new ObservablePoint(3,3),
+                        new ObservablePoint(4,2),
+                        new ObservablePoint(5,5)
+                    },
+                    PointGeometrySize = 3
+                },
+
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservablePoint>
+                    {
+                        new ObservablePoint(1,-1),
+                        new ObservablePoint(2,-4),
+                        new ObservablePoint(3,-2),
+                        new ObservablePoint(4,-3),
+                        new ObservablePoint(5,-6)
+                    },
+                    PointGeometrySize = 3
+                }
+
+            };
+        }
+    }
+}
